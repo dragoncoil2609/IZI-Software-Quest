ec16bd0b85354bd32bc1aa56d65ea7bb9f952bda
dragoncoil2609 <hoang100900@donga.eduvn>
2025-06-22 12:29:43 +0700

    Day 4_ Shipmovement and inptut mouse

---
+++ Assets/_Data/OurShipPrefab/ShipMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    [SerializeField] protected Vector3 worldPosition;
+    [SerializeField] protected float speed = 0.1f;
+
+    void FixedUpdate()
+    {
+        this.worldPosition = InputManager.instance.mouseWorldPos;
+        this.worldPosition.z = 0;
+
+        Vector3 newPos = Vector3.Lerp(transform.parent.position, worldPosition, this.speed);
+        transform.parent.position = newPos;
+    }
+}
