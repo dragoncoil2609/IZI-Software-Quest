68cf5790aff848fd48f3f05d77edf0868b7766c1
dragoncoil2609 <hoang100900@donga.eduvn>
2025-07-02 11:11:19 +0700

    Shipshongting and bullet

---
+++ Assets/_Data/OurShipPrefab/Ship/ShipShooting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooting : MonoBehaviour
+{
+    [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected Transform bulletPrefab;
+
+    private void FixedUpdate()
+    {
+        this.Shooting();
+    }
+
+    protected virtual void Shooting()
+    {
+        if (!this.isShooting) return;
+
+        Vector3 spawnPos = transform.parent.position;
+        Quaternion rotation = transform.parent.rotation;
+
+        Instantiate(this.bulletPrefab, spawnPos, rotation);
+        Debug.Log("Shooting");
+    }
+}
