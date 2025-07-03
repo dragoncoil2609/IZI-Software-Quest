c8eb50228128ecf8279f4ab6fb8cc06ade9dc90d
dragoncoil2609 <hoang100900@donga.eduvn>
2025-07-03 18:03:07 +0700

    fire bullet  1 or 2

---
+++ Assets/_Data/Ship/ShipShooting.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooting : MonoBehaviour
+{
+    [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected float shootDelay = 0.2f;
+    [SerializeField] protected float shootTimer = 0f;
+
+    void Update()
+    {
+        this.IsShooting();
+    }
+
+    private void FixedUpdate()
+    {
+        this.Shooting();
+    }
+
+    protected virtual void Shooting()
+    {
+        this.shootTimer += Time.fixedDeltaTime;
+
+        if (!this.isShooting) return;
+        if (this.shootTimer < this.shootDelay) return;
+        this.shootTimer = 0;
+
+        Vector3 spawnPos = transform.position;
+        Quaternion rotation = transform.parent.rotation;
+        //Transform newBullet = Instantiate(this.bulletPrefab, spawnPos, rotation);
+        Transform newBullet = BulletSpawner.Instance.Spawn(BulletSpawner.bulletTwo, spawnPos, rotation);
+        if (newBullet == null) return;
+
+        newBullet.gameObject.SetActive(true);
+        Debug.Log("Shooting");
+    }
+
+    protected virtual bool IsShooting()
+    {
+        this.isShooting = InputManager.Instance.OnFiring == 1;
+        return this.isShooting;
+    }
+}
